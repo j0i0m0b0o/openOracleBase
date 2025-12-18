41ddcfe452fd487b634ba85ae65040a226e5ba92
j0i0m0b0o <90911158+j0i0m0b0o@users.noreply.github.com>
2025-12-18 05:37:29 -0800

    Update IBounty.sol

--- src/interfaces/IBounty.sol
+++ src/interfaces/IBounty.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
 
+interface IBounty {
+    function createOracleBountyFwd(uint256, uint256, address, address, uint16, uint16, bool, uint256) external payable;
+    function recallBounty(uint256) external;
+    function editBounty(uint256, uint256) external;
+
+    struct Bounties {
+        uint256 totalAmtDeposited;
+        uint256 bountyStartAmt;
+        uint256 bountyClaimed;
+        uint256 start;
+        uint256 forwardStartTime;
+        address payable creator;
+        address editor;
+        uint16 bountyMultiplier;
+        uint16 maxRounds;
+        bool claimed;
+        bool recalled;
+        bool timeType;
+    }
+
+    function Bounty(uint256 id) external view returns (Bounties memory);
+
+}
